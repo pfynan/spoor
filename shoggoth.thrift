01d22216e840658904c2e1a93cb5682afc5aa9a5
Phillip Fynan <pfynan@gmail.com>
2014-04-29 16:25:29 -0400

    Updated shoggoth

--- shoggoth.thrift
+++ shoggoth.thrift
@@ -1,6 +1,6 @@
 struct Coordinates {
-    1: i16 phi,
-    2: i16 theta
+    1: i16 x,
+    2: i16 y
 }
 
 enum PointMode {
@@ -8,12 +8,38 @@ enum PointMode {
     AUTOMATIC
 }
 
-
-service Tracking {
-    void setMode(1: PointMode mode),
-    PointMode getMode(),
-    void setPos(1: i16 target),
-    Coordinates getActualPos(),
-    void setOnOff(1: bool state)
+enum LightStatus {
+    OVERHEAT,
+    ON,
+    OFF
 }
+
+enum MoveStatus {
+    RUNNING,
+    UNCAL,
+    CALLING,
+    SLEEPING,
+    TILT_OVERCUR,
+    TILT_FAULT,
+    PAN_OVERCUR,
+    PAN_FAULT
+}
+
+
+service Tracking
+    { void setMode(1: PointMode mode)
+    , PointMode getMode()
+
+    , void setPos(1: Coordinates coord)
+    , void setOnOff(1: bool state)
+    , void halt()
+    , void sleep()
+    , void wake()
+    , void setIntensity(1: byte intens)
+    , void calibrate()
+    , LightStatus getLightStatus()
+    , byte getIntensity()
+    , MoveStatus getMoveStatus()
+    , Coordinates getActualPos()
+    }
 
